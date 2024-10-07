94d8ee3967ba3eb793bc809b17bbcee202a7d7ce
hohlov-d <hohlov_d.o@mail.ru>
2024-10-08 04:12:40 +0500

    Add files via upload

---
+++ module_14_3.py
@@ -0,0 +1,111 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+kb_1 = InlineKeyboardMarkup()
+kb_2 = InlineKeyboardMarkup()
+button_1 = KeyboardButton(text='Рассчитать')
+button_2 = KeyboardButton(text='Информация')
+button_5 = KeyboardButton(text='Купить')
+button_3 = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
+button_4 = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+button_prod_1 = InlineKeyboardButton(text='БАД 1', callback_data='product_buying')
+button_prod_2 = InlineKeyboardButton(text='БАД 2', callback_data='product_buying')
+button_prod_3 = InlineKeyboardButton(text='БАД 3', callback_data='product_buying')
+button_prod_4 = InlineKeyboardButton(text='БАД 4', callback_data='product_buying')
+kb.row(button_1, button_2, button_5)
+kb_1.row(button_3, button_4)
+kb_2.row(button_prod_1, button_prod_2, button_prod_3, button_prod_4)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=kb)
+
+
+@dp.message_handler(text='Информация')
+async def inform(message):
+    await message.answer('Данный Бот помогает расчитать суточную норму каллорий для мужчин по заданным параметрам '
+                         'используя формулу Миффлина-Сан Жеора')
+
+
+@dp.message_handler(text='Рассчитать')
+async def main_menu(message):
+    await message.answer('Выберите опцию', reply_markup=kb_1)
+
+
+@dp.message_handler(text='Купить')
+async def get_buying_list(message):
+    with open('files/1.jpg', 'rb') as img1:
+        await message.answer_photo(img1, f'Название: БАД1 | Описание: описание1 | Цена: 100')
+    with open('files/2.png', 'rb') as img2:
+        await message.answer_photo(img2, f'Название: БАД2 | Описание: описание2 | Цена: 200')
+    with open('files/3.jpg', 'rb') as img3:
+        await message.answer_photo(img3, f'Название: БАД3 | Описание: описание3 | Цена: 300')
+    with open('files/4.jpg', 'rb') as img4:
+        await message.answer_photo(img4, f'Название: БАД4 | Описание: описание4 | Цена: 400')
+    await message.answer('Выберите продукт для покупки:', reply_markup=kb_2)
+
+@dp.callback_query_handler(text='product_buying')
+async def send_confirm_message(call):
+    await call.message.answer('Вы успешно приобрели продукт!')
+    await call.answer()
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+    await call.message.answer('Формула Миффлина-Сан Жеора для мужчин: '
+                              '10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5')
+    await call.answer()
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.callback_query_handler(text='calories')
+async def set_age(call):
+    await call.message.answer('Введите свой возраст:')
+    await call.answer()
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(first=float(message.text))
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(second=float(message.text))
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(third=float(message.text))
+    data = await state.get_data()
+    result = (10 * data['third']) + (6.25 * data['second']) - (5 * data['first']) + 5
+    await message.answer(f'Ваша норма каллорий {result}')
+    await state.finish()
+
+
+@dp.message_handler()
+async def all_message(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
